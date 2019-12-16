a666ebde50d5c8541a81a466763cd749fdb20d92
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2019-12-16 07:53:06 -0300

    add route

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -3,15 +3,18 @@ import { RouterModule } from 'nest-router';
 
 import { AdminModule } from './admin/module';
 import { AppModule } from './app/module';
+import { StockModule } from './stock/module';
 
 @Module({
   imports: [
     RouterModule.forRoutes([
       { path: '/admin', module: AdminModule },
-      { path: '/app', module: AppModule }
+      { path: '/app', module: AppModule },
+      { path: '/stock', module: StockModule }
     ]),
     AdminModule,
-    AppModule
+    AppModule,
+    StockModule
   ]
 })
 export class ApplicationModule {}
